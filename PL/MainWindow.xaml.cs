2918b3ea9860fc40f7662c0fb55a2a54858a150a
Dan Zilberstein <dzilbers@gmail.com>
2021-12-12 12:17:39 +0200

    Fix PL project location

---
+++ PL/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using BlApi;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal readonly IBL bl = BlFactory.GetBl();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
